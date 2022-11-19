8e3bb966e78706470e4ef5d88dea0f76a0ee58b2
Jonas Bechler <jonasbechler@gmail.com>
2022-11-19 16:58:23 +0100

    add session secret

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const fusionAuth_config = require('../fusionauth_config.json')
 
 config.port = config.login.port
 config.port_react = config.login.port_react
+config.session_secret = config.login.session_secret
 config.fusionauth = fusionAuth_config
 
 const frontend_dir = path.join(__dirname, "..", "..", "react", "build")
