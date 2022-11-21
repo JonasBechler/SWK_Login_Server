0db080c5846497d70747e4a0f0e686274db0b8e4
Jonas Bechler <jonasbechler@gmail.com>
2022-11-21 10:41:29 +0100

    correct dir

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ config.port_react = config.login.port_react
 config.session_secret = config.login.session_secret
 config.fusionauth = fusionAuth_config
 
-const frontend_dir = path.join(__dirname, "..", "..", "react", "build")
+const frontend_dir = path.join(__dirname, "..", "..", "SWK_Login_React", "build")
 
 var app = require('./main')(config, "", frontend_dir)
 
