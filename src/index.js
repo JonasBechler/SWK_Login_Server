3c9c66727714f86ad55a222051d2b864420f34cc
JonasBechler <jonasbechler@gmail.com>
2023-01-22 00:33:01 +0100

    bugfix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ config.port_react = config.login.port_react
 config.session_secret = config.login.session_secret
 config.fusionauth = config.login.fusionauth
 
-const frontend_dir = path.join(__dirname, "..", "..", "SWK_Login_React", "build")
+const frontend_dir = path.join(__dirname, "..", "SWK_Login_React", "build")
 
 var app = require('./main')(config, "", frontend_dir)
 
