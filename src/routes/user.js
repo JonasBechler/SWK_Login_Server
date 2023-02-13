2554ea33f1b536b5bb1efa1becf076e659d0e9ca
JonasBechler <jonasbechler@gmail.com>
2023-02-13 18:45:47 +0100

    bugfix cet

--- src/routes/user.js
+++ src/routes/user.js
@@ -35,8 +35,8 @@ module.exports = function( config ) {
 								"email": ""+introspectResponse.email,
 								"email_verified": introspectResponse.email_verified?"verifiziert":"nicht verifiziert",
 								"knlogin_id": ""+introspectResponse.sub,
-								"iat_date":""+iat_date.getHours()+":"+iat_date.getMinutes()+":"+iat_date.getSeconds(),
-								"exp_date":""+exp_date.getHours()+":"+exp_date.getMinutes()+":"+exp_date.getSeconds(),		
+								"iat_date":""+(iat_date.getHours()+1)+":"+iat_date.getMinutes()+":"+iat_date.getSeconds(),
+								"exp_date":""+(exp_date.getHours()+1)+":"+exp_date.getMinutes()+":"+exp_date.getSeconds(),		
 							}
 						}
 
